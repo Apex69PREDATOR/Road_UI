a17c43eaa89c1e57817b63a00d16ea2898c30984
Apex <arpana036@gmail.com>
2025-03-21 23:52:56 +0530

    comment

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -75,6 +75,7 @@ const BlogPosts = (props) => {
            <p>{val.description}</p>
            <div className="likes flex justify-end items-center border-t-[1.2px] border-black h-[6%] w-[90%]">
             <p className='p-2'>{liked[val._id]}</p>
+            <i className='fa-regular fa-comment text-2xl cursor-pointer hover:scale-125 transition-transform duration-200'></i>
            <i onClick={(e)=>{update_like(e.target.parentElement.parentElement.id)}} className={`fa-${likedarr.includes(val._id)?'solid':'regular'} fa-heart text-2xl cursor-pointer hover:scale-125 transition-transform duration-200 text-red-500`} ></i>
            </div>
           </div>

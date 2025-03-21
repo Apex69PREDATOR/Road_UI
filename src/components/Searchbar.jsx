dd36312cd24894704d0b8a8d394c0313ff648ea9
Apex <arpana036@gmail.com>
2025-03-21 14:19:07 +0530

    Loading.. button

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,7 +4,7 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 const Searchbar = (props) => {
   return (
     <>
-    <FontAwesomeIcon icon={faMagnifyingGlass} className={`absolute opacity-40 text-black left-[${props.name?'20':'35'}%] top-[35%] sm:left-[23%] sm:top-[42%]`} />
+    <FontAwesomeIcon icon={faMagnifyingGlass} className={`absolute opacity-40 text-black left-[27%] top-[35%] sm:left-[23%] sm:top-[42%]`} />
     <input type="text" className='bg-transparent border-b-2 border-black sm:ml-[15%] sm:mr-[4%]  h-10 w-[25%] sm:w-[10%] text-black text-[1.1rem]' placeholder=' Search....'  />
     </> 
   )

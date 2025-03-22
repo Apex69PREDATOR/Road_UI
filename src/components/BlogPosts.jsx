f4be956a3cf780d4753ea14a6177022e5aff055f
Apex <arpana036@gmail.com>
2025-03-22 21:59:50 +0530

    like issue fixed

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -69,7 +69,7 @@ const BlogPosts = (props) => {
       <h2 className='text-3xl text-blue'>{message}</h2>
         {blogdata.length?blogdata.map(val=>{
           
-          return <><div  className='blog-card bg-white relative flex flex-col justify-between [&_*:not(img,b,span,i)]:ml-[5%] rounded border-black w-[90%] h-[70vh] md:h-[96vh] md:w-[60%]' id={val._id}>
+          return <><div  className='blog-card bg-white relative flex flex-col justify-between [&_*:not(img,b,span,i)]:ml-[5%] rounded border-black w-[95%] h-[70vh] md:h-[97vh] md:w-[59%]' id={val._id}>
             <img src={val.image_url} className='h-[60%] md:h-[66%] w-[100%]' alt="construction image" />
            <p className='blog-usrname'> <span className='font-semibold font-sans'> {val.name}</span> <br /> {month[val.date.split('T')[0].split('-')[1]]} {val.date.split('T')[0].split('-')[2]}, {val.date.split('T')[0].split('-')[0]} <span className='text-[0.3em]'>●</span> <span className='p-[0.7%] md:p-1 rounded bg-[rgb(66,68,67)] text-white'>{val.timePassed.year>0 && val.timePassed.year +' years'  } {val.timePassed.month>0 && val.timePassed.month +' months'} {val.timePassed.day>0 && val.timePassed.day +' d'} {val.timePassed.hour>0 && val.timePassed.hour +' hr'} {val.timePassed.min>0 && val.timePassed.min +' min'} {(val.timePassed.year==0 && val.timePassed.month==0 && val.timePassed.day==0 && val.timePassed.hour==0 && val.timePassed.min==0)?'just now':'ago'}</span></p>
            <p className='text-[1.15em] md:text-[1.5em] font-semibold b-heading'>{val.location} <br />

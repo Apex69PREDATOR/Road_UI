5b750d96a14620f3c010348dcb4312af575e4dcb
Apex <arpana036@gmail.com>
2025-03-23 01:59:01 +0530

    coment box fixed

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -86,9 +86,9 @@ const BlogPosts = (props) => {
           
           </div>
           {
-            comment[val._id] && <div className='comment bg-white rounded h-[20vh] w-[90%] md:w-[60%]'>{commentarr[val._id]?commentarr[val._id].map(val=>{
-              return <div className='flex flex-col'><p>{val.name}</p><p>{val.comment}</p></div>
-            }):<p>No comments till now</p>}</div>
+            comment[val._id] && <div className='comment relative bg-white rounded h-[20vh] w-[90%] md:w-[60%]'>{commentarr[val._id]?commentarr[val._id].map(val=>{
+              return <div className='flex flex-col h-[20%]'><p>{val.name}</p><p>{val.comment}</p></div>
+            }):<p>No comments till now</p>} <input className='absolute bottom-[5%]' type="text" placeholder='Comment something..' /> </div>
            }
           </>
         }):<p>No Blogs available</p>}
